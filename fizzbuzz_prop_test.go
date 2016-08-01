d8ff0806fe623d0f9d710afd229a78b5b005bf03
Caitie McCaffrey <caitiem20@gmail.com>
2016-07-31 21:52:24 -0700

    adding property based tests (#4)

---
+++ fizzbuzz_prop_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "github.com/leanovate/gopter"
+  "github.com/leanovate/gopter/gen"
+  "github.com/leanovate/gopter/prop"
+  "testing"
+)
+
+func Test_ValidateFizzBuzz(t *testing.T) {
+  parameters := gopter.DefaultTestParameters()
+  parameters.MinSuccessfulTests = 10000
+  properties := gopter.NewProperties(parameters)
+
+  properties.Property("FizzBuzz Returns Correct String", prop.ForAll(
+    func(num int) bool {
+
+      str := fizzBuzz(num)
+
+      switch str {
+      case "Fizz":
+        return (num % 3 == 0) && !(num % 5 == 0)
+      case "Buzz":
+        return (num % 5 == 0) && !(num % 3 == 0)
+      case "FizzBuzz":
+        return (num % 3 == 0) && (num % 5 == 0)
+      default:
+        return !(num % 3 == 0) && !(num % 5 == 0)
+      }
+
+    },
+    gen.Int(),
+  ))
+
+  properties.TestingRun(t)
+}
--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
   "fmt"
+  "log"
+  "strconv"
+  "os"
 )
 
 func main() {
-    fmt.Printf(getMessage())
+    num, err := strconv.Atoi(os.Args[1])
+
+    if err != nil {
+      log.Fatal("Input must be an integer")
+    }
+    str := fizzBuzz(num)
+    fmt.Println(str)
 }
 
-func getMessage() string {
-  return "hello, world"
-}
+func fizzBuzz(num int) string {
 
+  div3 := (num % 3) == 0
+  div5 := (num % 5) == 0
+
+  if div3 && div5 {
+    return "FizzBuzz"
+  } else if div3 {
+    return "Fizz"
+  } else if div5 {
+    return "Buzz"
+  } else {
+    return strconv.Itoa(num)
+  }
+}
